885447b6ba59d8332e6e2ba9ce9a00546e8ce67e
Tomek Mrugalski <tomasz@isc.org>
2020-04-16 21:53:41 +0200

    [#164] AppInitService unit-test fixed!

--- webui/src/app/app-init.service.spec.ts
+++ webui/src/app/app-init.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing'
 
 import { AppInitService } from './app-init.service'
+import { UsersService } from './backend'
+import { Component } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('AppInitService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}))
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [UsersService, HttpClient, HttpHandler]
+    }))
 
     it('should be created', () => {
         const service: AppInitService = TestBed.inject(AppInitService)
